c1dcc35ab006f466586334d6e916c0cbef6f1389
Jason Pellerin <jpellerin@gmail.com>
2007-04-08 04:33:05 +0000

    Added some doctests to errorclass

--- nose/plugins/errorclass.py
+++ nose/plugins/errorclass.py
@@ -1,3 +1,91 @@
+"""
+ErrorClass Plugins
+------------------
+
+ErrorClass plugins provide an easy way to add support for custom
+handling of particular classes of exceptions.
+
+An ErrorClass plugin defines one or more ErrorClasses and how each is
+handled and reported on. Each error class is stored in a different
+attribute on the result, and reported separated. Each error class must
+indicate the exceptions that fall under that class, the label to use
+for reporting, and whether exceptions of the class should be
+considered as failures for the whole test run.
+
+ErrorClasses use a declarative syntax. Assign an ErrorClass to the
+attribute you wish to add to the result object, defining the
+exceptions, label and isfailure attributes. For example, to declare an
+ErrorClassPlugin that defines TodoErrors (and subclasses of TodoError)
+as an error class with the label 'TODO' that is considered a failure,
+do this:
+
+    >>> class Todo(Exception):
+    ...     pass
+    >>> class TodoError(ErrorClassPlugin):
+    ...     todo = ErrorClass(Todo, label='TODO', isfailure=True)
+
+The MetaErrorClass metaclass translates the ErrorClass declarations
+into the tuples used by the error handling and reporting functions in
+the result. This is an internal format and subject to change; you
+should always use the declarative syntax for attaching ErrorClasses to
+an ErrorClass plugin.
+    
+    >>> TodoError.errorClasses # doctest: +ELLIPSIS
+    ((<class ...Todo at ...>, ('todo', 'TODO', True)),)
+
+Let's see the plugin in action. First some boilerplate.
+
+    >>> import sys
+    >>> import unittest
+    >>> from nose.config import Config
+    >>> from nose.plugins.manager import PluginManager
+
+    >>> buf = unittest._WritelnDecorator(sys.stdout)
+
+Now define a test case that raises a Todo.
+
+    >>> class TestTodo(unittest.TestCase):
+    ...     def runTest(self):
+    ...         raise Todo("I need to test something")
+    >>> case = TestTodo()
+
+Prepare the result using our plugin. Normally this happens during the
+course of test execution within nose.
+
+    >>> plugin = TodoError()
+    >>> result = unittest._TextTestResult(stream=buf,
+    ...                                   descriptions=0, verbosity=2)
+    >>> plugin.prepareTestResult(result)
+
+Now run the test. TODO is printed.
+
+    >>> case(result) # doctest: +ELLIPSIS
+    runTest (....TestTodo) ... TODO
+
+Errors and failures are empty, but todo has our test
+
+    >>> result.errors
+    []
+    >>> result.failures
+    []
+    >>> result.todo # doctest: +ELLIPSIS
+    [(<....TestTodo testMethod=runTest>, '...Todo: I need to test something\\n')]
+    >>> result.printErrors() # doctest: +ELLIPSIS
+    <BLANKLINE>
+    ======================================================================
+    TODO: runTest (....TestTodo)
+    ----------------------------------------------------------------------
+    Traceback (most recent call last):
+    ...
+    Todo: I need to test something
+    <BLANKLINE>
+
+Since we defined a Todo as a failure, the run was not successful.
+
+    >>> result.wasSuccessful()
+    False
+"""
+
 from new import instancemethod
 from nose.plugins.base import Plugin
 from nose.result import TextTestResult
@@ -6,6 +94,9 @@ from nose.util import odict
 
 
 class MetaErrorClass(type):
+    """Metaclass for ErrorClassPlugins that allows error classes to be
+    set up in a declarative manner.
+    """
     def __init__(self, name, bases, attr):
         errorClasses = []
         for name, detail in attr.items():
@@ -89,3 +180,6 @@ def wassuccessful_patch(result):
         TextTestResult.wasSuccessful.im_func, result, result.__class__)
 
     
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
