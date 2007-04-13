af063f4bdba154b769f3df8a9e9b7cd0d6222667
Jason Pellerin <jpellerin@gmail.com>
2007-04-13 18:33:31 +0000

    Added array-attribute tests

--- functional_tests/support/att/test_attr.py
+++ functional_tests/support/att/test_attr.py
@@ -3,18 +3,19 @@ import unittest
 def test_one():
     pass
 test_one.a = 1
+test_one.d = [1, 2]
 
 
 def test_two():
     pass
 test_two.a = 1
 test_two.c = 20
-
+test_two.d = [2, 3]
 
 def test_three():
     pass
 test_three.b = 1
-
+test_three.d = [1, 3]
 
 class TestClass:
     a = 1
--- functional_tests/test_attribute_plugin.py
+++ functional_tests/test_attribute_plugin.py
@@ -73,6 +73,71 @@ class TestAttributeValue(PluginTester, unittest.TestCase):
         assert 'test_case_one' in self.output
         assert 'test_case_three' in self.output
 
+class TestAttributeArray(PluginTester, unittest.TestCase):
+    activate = "-a d=2"
+    args = ['-v']
+    plugins = [AttributeSelector()]
+    suitepath = os.path.join(support, 'att')
+
+    def runTest(self):
+        print '*' * 70
+        print str(self.output)
+        print '*' * 70
+        
+        assert 'test_attr.test_one ... ok' in self.output
+        assert 'test_attr.test_two ... ok' in self.output
+        assert 'test_attr.test_three ... ok' not in self.output
+        assert 'TestClass.test_class_one ... ok' not in self.output
+        assert 'TestClass.test_class_two ... ok' not in self.output
+        assert 'TestClass.test_class_three ... ok' not in self.output
+        assert 'test_case_two' not in self.output
+        assert 'test_case_one' not in self.output
+        assert 'test_case_three' not in self.output
+
+
+class TestAttributeArrayAnd(PluginTester, unittest.TestCase):
+    activate = "-a d=1,d=2"
+    args = ['-v']
+    plugins = [AttributeSelector()]
+    suitepath = os.path.join(support, 'att')
+
+    def runTest(self):
+        print '*' * 70
+        print str(self.output)
+        print '*' * 70
+        
+        assert 'test_attr.test_one ... ok' in self.output
+        assert 'test_attr.test_two ... ok' not in self.output
+        assert 'test_attr.test_three ... ok' not in self.output
+        assert 'TestClass.test_class_one ... ok' not in self.output
+        assert 'TestClass.test_class_two ... ok' not in self.output
+        assert 'TestClass.test_class_three ... ok' not in self.output
+        assert 'test_case_two' not in self.output
+        assert 'test_case_one' not in self.output
+        assert 'test_case_three' not in self.output
+
+class TestAttributeArrayOr(PluginTester, unittest.TestCase):
+    activate = "-v"
+    args = ['-a', 'd=1', '-a', 'd=2']
+    plugins = [AttributeSelector()]
+    suitepath = os.path.join(support, 'att')
+
+    def runTest(self):
+        print '*' * 70
+        print str(self.output)
+        print '*' * 70
+        
+        assert 'test_attr.test_one ... ok' in self.output
+        assert 'test_attr.test_two ... ok' in self.output
+        assert 'test_attr.test_three ... ok' in self.output
+        assert 'TestClass.test_class_one ... ok' not in self.output
+        assert 'TestClass.test_class_two ... ok' not in self.output
+        assert 'TestClass.test_class_three ... ok' not in self.output
+        assert 'test_case_two' not in self.output
+        assert 'test_case_one' not in self.output
+        assert 'test_case_three' not in self.output
+        
+
 if compat_24:
     class TestAttributeEval(PluginTester, unittest.TestCase):
         activate = "-A c>20"
