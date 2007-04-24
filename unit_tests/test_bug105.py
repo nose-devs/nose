02a57ae8e989b7bc697cef06c1173c9208a2e103
Jason Pellerin <jpellerin@gmail.com>
2007-04-24 21:19:01 +0000

    Fixed failing unit test imported from trunk

--- unit_tests/test_bug105.py
+++ unit_tests/test_bug105.py
@@ -4,13 +4,12 @@ import unittest
 class TestBug105(unittest.TestCase):
 
     def test_load_in_def_order(self):
-        from nose.config import Config
         from nose.loader import TestLoader
 
         where = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                              'support', 'bug105'))
-        c = Config()
-        l = TestLoader(conf=c)
+
+        l = TestLoader()
         testmod = l.loadTestsFromDir(where).next()
         print testmod
         testmod.setUp()
