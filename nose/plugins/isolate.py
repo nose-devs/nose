30f4d378d7a72fed032cd3357ca80002df7064e0
Jason Pellerin <jpellerin@gmail.com>
2007-04-25 16:03:14 +0000

    Implemented loadTestsFromNames hook in isolate plugin.

--- nose/plugins/isolate.py
+++ nose/plugins/isolate.py
@@ -1,24 +1,35 @@
 """Use the isolation plugin with --with-isolation or the
-NOSE_WITH_ISOLATION environment variable to clean sys.modules before
-and after each test module is executed.
+NOSE_WITH_ISOLATION environment variable to clean sys.modules after
+each test module is loaded and executed.
 
-The isolation module is in effect the same as including the following
-functions in every test module and test package __init__.py:
+The isolation module is in effect similar to wrapping the following
+functions around the import and execution of each test module::
 
-    >>> def setup(module):
-    ...     module._mods = sys.modules.copy()
+    def setup(module):
+        module._mods = sys.modules.copy()
     
-    >>> def teardown(module):
-    ...     to_del = [ m for m in sys.modules.keys() if m not in
-    ...                module._mods ]
-    ...     for mod in to_del:
-    ...         del sys.modules[mod]
-    ...     sys.modules.update(module._mods)
+    def teardown(module):
+        to_del = [ m for m in sys.modules.keys() if m not in
+                   module._mods ]
+        for mod in to_del:
+            del sys.modules[mod]
+        sys.modules.update(module._mods)
 
-PLEASE NOTE that this plugin MAY NOT be used in combination with the
-coverage plugin, as coverage data and state will be flushed after each
-test module is run.
+Isolation works only during lazy loading. In normal use, this is only
+during discovery of modules within a directory, where the process of
+importing, loading tests and running tests from each module is
+encapsulated in a single loadTestsFromName call. This plugin
+implements loadTestsFromNames to force the same lazy-loading there,
+which allows isolation to work in directed mode as well as discovery,
+at the cost of some efficiency: lazy-loading names forces full context
+setup and teardown to run for each name, defeating the grouping that
+is normally used to ensure that context setup and teardown are run the
+fewest possible times for a given set of names.
 
+PLEASE NOTE that this plugin should not be used in conjunction with
+other plugins that assume that modules once imported will stay
+imported; for instance, it may cause very odd results when used with
+the coverage plugin.
 """
 
 import logging
@@ -37,6 +48,7 @@ class IsolationPlugin(Plugin):
     package runs to its state before the test. PLEASE NOTE that this
     plugin may not be used with the coverage plugin.
     """
+    score = 10 # I want to be last
     name = 'isolation'
 
     def configure(self, options, conf):
@@ -54,30 +66,32 @@ class IsolationPlugin(Plugin):
         it was in when mod stack was pushed.
         """
         mods = self._mod_stack.pop()
-        to_del = [ m for m in sys.modules.keys() if
-                       m not in mods ]
+        to_del = [ m for m in sys.modules.keys() if m not in mods ]
         if to_del:
             log.debug('removing sys modules entries: %s', to_del)
             for mod in to_del:
                 del sys.modules[mod]
         sys.modules.update(mods)
-    
-#     def startTest(self, test):
-#         """Save the state of sys.modules if we're starting a test module
-#         """
-#         if isinstance(test, TestModule):
-#             log.debug('isolating sys.modules changes in %s', test)
-#             self._mods = sys.modules.copy()
-            
-#     def stopTest(self, test):
-#         """Restore the saved state of sys.modules if we're ending a test module
-#         """
-#         if isinstance(test, TestModule):            
-#             to_del = [ m for m in sys.modules.keys() if
-#                        m not in self._mods ]
-#             if to_del:
-#                 log.debug('removing sys modules entries: %s', to_del)
-#                 for mod in to_del:
-#                     del sys.modules[mod]
-#             sys.modules.update(self._mods)
+
+    def loadTestsFromNames(self, names, module=None):
+        """Create a lazy suite that calls beforeContext and afterContext
+        around each name. The side-effect of this is that full context
+        fixtures will be set up and torn down around each test named.
+        """
+        # Fast path for when we don't care
+        if not names or len(names) == 1:
+            return 
+        loader = self.loader
+        plugins = self.conf.plugins
+        def lazy():
+            for name in names:
+                plugins.beforeContext()
+                yield loader.loadTestsFromName(name, module=module)
+                plugins.afterContext()
+        return (loader.suiteClass(lazy), [])
+
+    def prepareTestLoader(self, loader):
+        """Get handle on test loader so we can use it in loadTestsFromNames.
+        """
+        self.loader = loader
 
