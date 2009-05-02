3adce92a0bb1b8ef06642ba61c850e47c8e07952
Jason Pellerin <jpellerin@gmail.com>
2009-05-02 14:04:06 -0400

    pypi info updated

--- setup.py
+++ setup.py
@@ -31,8 +31,7 @@ setup(
     version = VERSION,
     author = 'Jason Pellerin',
     author_email = 'jpellerin+nose@gmail.com',
-    description = ('A unittest extension offering automatic test suite '
-                   'discovery and easy test authoring'),
+    description = ('nose extends unittest to make testing easier'),
     long_description = \
     """nose extends the test loading and running features of unittest, making
     it easier to write, find and run tests.
@@ -42,19 +41,17 @@ setup(
     (like "test_this" or "functional_test" or "TestClass" but not
     "libtest"). Test output is similar to that of unittest, but also includes
     captured stdout output from failing tests, for easy print-style debugging.
-                        
+
     These features, and many more, are customizable through the use of
     plugins. Plugins included with nose provide support for doctest, code
     coverage and profiling, flexible attribute-based test selection,
     output capture and more. More information about writing plugins may be 
-    found on the wiki, here:
-    http://code.google.com/p/python-nose/wiki/WritingPlugins, and in the nose
-    API documentation, here:
-    http://somethingaboutorange.com/mrl/projects/nose/doc/
-    
+    found on in the nose API documentation, here:
+    http://somethingaboutorange.com/mrl/projects/nose/
+
     If you have recently reported a bug marked as fixed, or have a craving for
-    the very latest, you may want the development version instead:
-    http://python-nose.googlecode.com/svn/trunk#egg=nose-dev
+    the very latest, you may want the unstable development version instead:
+    http://bitbucket.org/jpellerin/nose/get/tip.gz#egg=nose-dev
     """,
     license = 'GNU LGPL',
     keywords = 'test unittest doctest automatic discovery',
@@ -67,7 +64,7 @@ setup(
                          'examples/*.py',
                          'examples/*/*.py']},
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Natural Language :: English',
