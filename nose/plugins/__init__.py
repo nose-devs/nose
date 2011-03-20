512f8369d69daca633781bf3aa27f7a71d3b7c11
kumar <kumar.mcmillan@gmail.com>
2011-03-20 01:48:11 -0500

    Adds a Hello World plugin to the documentation

--- nose/plugins/__init__.py
+++ nose/plugins/__init__.py
@@ -12,6 +12,35 @@ reporting. There are two basic rules for plugins:
   this class is for documentary purposes only; plugins may not subclass
   IPluginInterface.
 
+Hello World
+===========
+
+Here's a basic plugin.  It doesn't do much so read on for more ideas or dive
+into the :doc:`IPluginInterface <interface>` to see all available hooks.
+
+.. code-block:: python
+
+    import logging
+    import os
+
+    from nose.plugins import Plugin
+
+    log = logging.getLogger('nose.plugins.helloworld')
+
+    class HelloWorld(Plugin):
+        name = 'helloworld'
+
+        def options(self, parser, env=os.environ):
+            super(HelloWorld, self).options(parser, env=env)
+
+        def configure(self, options, conf):
+            super(HelloWorld, self).configure(options, conf)
+            if not self.enabled:
+                return
+
+        def finalize(self, result):
+            log.info('Hello pluginized world!')
+
 Registering
 ===========
 
