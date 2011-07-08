ce179d4bc6df51f5052f7c86abd7c6b2caa6bb42
kumar <kumar.mcmillan@gmail.com>
2011-07-08 10:46:10 -0500

    per discussion, 1.0.1 will actually be 1.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-VERSION = '1.0.1'
+VERSION = '1.1.0'
 py_vers_tag = '-%s.%s' % sys.version_info[:2]
 
 test_dirs = ['functional_tests', 'unit_tests', os.path.join('doc','doc_tests'), 'nose']
