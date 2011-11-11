c3a9d056a88ffd05330d702192434dae7b23e385
Brendan McCollam <bmccollam@leapfrogonline.com>
2011-11-11 11:45:11 -0600

    Updated VERSION, CHANGELOG, and AUTHORS

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-VERSION = '1.1.3'
+VERSION = '1.1.4'
 py_vers_tag = '-%s.%s' % sys.version_info[:2]
 
 test_dirs = ['functional_tests', 'unit_tests', os.path.join('doc','doc_tests'), 'nose']
