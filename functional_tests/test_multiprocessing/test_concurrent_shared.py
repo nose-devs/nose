2af3963f958f385fbb68a13962d86f6a4d1735f4
myint <myint@macbookpro.local>
2012-05-13 08:26:57 -0700

    Add test case with negative "--processes" value
    This should result in automatic selection of processes based on CPU
    count rather than hanging.

--- functional_tests/test_multiprocessing/test_concurrent_shared.py
+++ functional_tests/test_multiprocessing/test_concurrent_shared.py
@@ -11,3 +11,13 @@ class TestConcurrentShared(MPTestBase):
         assert 'Ran 2 tests in 1.' in self.output, "make sure two tests use 1.x seconds (no more than 2 seconsd)"
         assert str(self.output).strip().endswith('OK')
 
+
+class TestConcurrentSharedWithAutomaticProcessesCount(MPTestBase):
+    """Make sure negative numbers are handled gracefully."""
+    processes = -1
+    suitepath = os.path.join(os.path.dirname(__file__), 'support',
+                             'concurrent_shared')
+
+    def runTest(self):
+        assert 'Ran 2 tests in 1.' in self.output, "make sure two tests use 1.x seconds (no more than 2 seconsd)"
+        assert str(self.output).strip().endswith('OK')
