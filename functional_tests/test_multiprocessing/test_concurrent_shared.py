36f357c1b5275bda269d1cefcf2bd3bba5fb3189
John Szakmeister <john@szakmeister.net>
2013-02-08 17:03:11 -0500

    Finish the setUp() process in TestConcurrentShared.
    Not sure how this was missed, but we need to call setUp() on the base
    class in order to get the output capturing.

--- functional_tests/test_multiprocessing/test_concurrent_shared.py
+++ functional_tests/test_multiprocessing/test_concurrent_shared.py
@@ -17,6 +17,10 @@ class TestConcurrentShared(MPTestBase):
         if is_pypy:
             raise unittest.SkipTest('pypy warm-up is too slow; skipping')
 
+        # Need to call the base's setUp() routine to get the necessary output
+        # capturing.
+        MPTestBase.setUp(self)
+
     def runTest(self):
         assert 'Ran 2 tests in 1.' in self.output, "make sure two tests use 1.x seconds (no more than 2 seconsd)"
         assert str(self.output).strip().endswith('OK')
