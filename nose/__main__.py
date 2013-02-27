53b581dc676648908660d884c0f042249f815fe1
Stefano Rivera <stefano@rivera.za.net>
2013-02-27 16:23:16 +0200

    Use sys.executable instead of a hard-coded 'python'

--- nose/__main__.py
+++ nose/__main__.py
@@ -3,6 +3,6 @@ import sys
 from nose.core import run_exit
 
 if sys.argv[0].endswith('__main__.py'):
-    sys.argv[0] = 'python -m nose'
+    sys.argv[0] = '%s -m nose' % sys.executable
 
 run_exit()
