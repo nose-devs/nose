db5f2dea5b0301d5d6d1bb621860de94ca1e33fc
Bernard Paulus <bprecyclebin@gmail.com>
2013-06-25 22:28:10 +0200

    convenient launcher for windows

---
+++ bin/nosetests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Convenient launcher for windows
+
+Takes advantage of the association of .py files with the python interpreter
+"""
+
+from nose import main
+
+if __name__ == '__main__':
+    main()
