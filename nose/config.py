17ebf681d223ba234a5c78ae1e400c74b130d368
Ken Harris <kengruven@gmail.com>
2015-09-24 09:01:15 -0700

    Update Config class docstring for 'testMatch'

--- nose/config.py
+++ nose/config.py
@@ -147,7 +147,7 @@ class Config(object):
 
       self.env = env = kw.pop('env', {})
       self.args = ()
-      self.testMatch = re.compile(r'(?:\b|_)[Tt]est')
+      self.testMatch = re.compile(env.get('NOSE_TESTMATCH', r'(?:\b|_)[Tt]est'))
       self.addPaths = not env.get('NOSE_NOPATH', False)
       self.configSection = 'nosetests'
       self.debug = env.get('NOSE_DEBUG')
