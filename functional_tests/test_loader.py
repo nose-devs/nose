84bb82245d10798825439dc13846eb0538d84239
Tomas Orsava <torsava@redhat.com>
2016-12-12 14:35:50 +0100

    Fix compatibility with Python 3.6
    Python 3.6 returns ModuleNotFoundError instead of the previous ImportError.

--- functional_tests/test_loader.py
+++ functional_tests/test_loader.py
@@ -372,7 +372,7 @@ class TestNoseTestLoader(unittest.TestCase):
         assert res.errors, "Expected errors but got none"
         assert not res.failures, res.failures
         err = res.errors[0][0].test.exc_class
-        assert err is ImportError, \
+        assert issubclass(err, ImportError), \
             "Expected import error, got %s" % err
 
     def test_load_nonsense_name(self):
